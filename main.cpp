53d5bc567a3c04e80d2edfc13e181d0e67443a60
Harrison-Choww <124001270+Harrison-Choww@users.noreply.github.com>
2023-02-23 11:56:03 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "Iterator.hpp"
+#include "menu.hpp"
+#include "window.hpp"
+#include "task.hpp"
+#include "component.hpp"
+#include <iostream>
+#include <string>
+//#include "menu.cpp"
+using namespace std;
+
+int main() {
+    /* string id, date, desc;
+     cout << "Enter id of your Schedules" << endl;
+     cin >> id;
+     cout << "Enter description for your Schedules" << endl;
+     cin >> desc;
+     cout << "Enter date for your schedules" << endl;
+     cin >> date;*/
+
+    Menu m;
+    m.menu();
+
+    return 0;
+}
